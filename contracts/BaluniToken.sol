4f484b511d83cef1e4d7c079fbfcd01b3399df72
scobru <scobru1988@gmail.com>
2024-05-30 17:42:37 +0200

    fresh deploy

--- contracts/BaluniToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.8.25;
-
-import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
-
-contract BaluniToken is ERC20Upgradeable {
-    function initialize(string memory name, string memory symbol) public initializer {
-        __ERC20_init(name, symbol);
-    }
-}
